242cfe26fc939684b20cffcd1530c0ba5b2ca048
kevin-sys <kevingcnemexix@gmail.com>
2021-04-08 20:10:19 -0500

    Commit create component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CrearComponent } from './Usuario/crear/crear.component';
+import { EditarComponent } from './Usuario/editar/editar.component';
+import { ListadoComponent } from './Usuario/listado/listado.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'crear', component:CrearComponent},
+  {path:'editar', component:EditarComponent},
+  {path:'listado', component:ListadoComponent} 
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ListadoComponent } from './Usuario/listado/listado.component';
+import { EditarComponent } from './Usuario/editar/editar.component';
+import { CrearComponent } from './Usuario/crear/crear.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListadoComponent,
+    EditarComponent,
+    CrearComponent
   ],
   imports: [
     BrowserModule,
